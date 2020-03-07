e34b20772b3dc5aafef60711888a2026d7f3eb6b
Viktor Nordling <vnordling@palantir.com>
2020-03-07 23:29:27 +0900

    Handle unknown timezones.

--- src/main/kotlin/com/github/viktornordling/kvitter/Search.kt
+++ src/main/kotlin/com/github/viktornordling/kvitter/Search.kt
@@ -8,8 +8,12 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import java.io.File
 import java.nio.file.Paths
+import java.text.DateFormatSymbols
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.Locale
+import java.util.TimeZone
+import java.util.spi.TimeZoneNameProvider
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class User(val screenName: String)
@@ -24,18 +28,34 @@ class TweetList : TypeReference<List<Tweet>>()
 
 class Search: CliktCommand(help = "Search tweets") {
     private val searchTerm by argument(name = "SEARCH-TERM", help = "Term you want to search for")
+    private val knownTimezones = TimeZone.getAvailableIDs().toSet()
 
     override fun run() {
         println("Searching for '$searchTerm'")
         val mapper = ObjectMapper().registerModule(KotlinModule())
         val files = Paths.get("archive").toFile().walkTopDown().filter { f -> f.name.endsWith(".json") }
-        // Format is "Wed Mar 20 10:43:53 JST 2019".
+        // Format is "Wed Mar 20 10:43:53 JST 2019"
         val formatter = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss z yyyy")
         val matchingTweets = files.map { file ->
             val tweets: List<Tweet> = mapper.readValue(file, TweetList())
             tweets.filter { tweet -> tweet.text.toLowerCase().contains(searchTerm) }
         }.flatten()
-        matchingTweets.sortedBy { LocalDate.parse(it.createdAt, formatter) }.forEach { tweet -> println("Found match: ${tweet.createdAt}: ${tweet.text}, \nlink: https://twitter.com/${tweet.user.screenName}/status/${tweet.id}") }
+        matchingTweets
+                .map { fixTimezone(it) }
+                .sortedBy { LocalDate.parse(it.createdAt, formatter) }.forEach { tweet -> println("Found match: ${tweet.createdAt}: ${tweet.text}, \nlink: https://twitter.com/${tweet.user.screenName}/status/${tweet.id}") }
+    }
+
+    fun fixTimezone(tweet: Tweet): Tweet {
+        // Because twitter is horrible, the timezone is sometimes bogus (like PHT), so convert any bogus
+        // timezones to the user's local timezone.
+        val parts = tweet.createdAt.split(" ")
+        if (parts.size == 6) {
+            val timezone = tweet.createdAt.split(" ").takeLast(2).first()
+            if (!knownTimezones.contains(timezone)) {
+                return tweet.copy(createdAt = tweet.createdAt.replace(timezone, TimeZone.getDefault().id))
+            }
+        }
+        return tweet
     }
 }
 
---
+++ src/test/kotlin/twitter/searcher/client/SearchTest.kt
@@ -0,0 +1,17 @@
+package twitter.searcher.client
+
+import com.github.viktornordling.kvitter.Search
+import com.github.viktornordling.kvitter.Tweet
+import com.github.viktornordling.kvitter.User
+import org.junit.Test
+
+class SearchTest {
+
+    @Test
+    fun testFixTimezone() {
+        val someUser = User(screenName = "someone")
+        val tweet = Tweet(createdAt = "Sat Apr 13 19:13:56 PHT 2019", text = "some text", id = "some id", user = someUser, URLEntities = listOf(), retweet = false)
+        val fixedTweet = Search().fixTimezone(tweet)
+        println(fixedTweet.createdAt)
+    }
+}
